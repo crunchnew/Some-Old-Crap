5a4f1a5f2325e7f9d50be2324f899dcaecc10ca6
crunchnew <crunchnew@gmail.com>
2018-03-12 12:02:19 +0100

    new main

---
+++ src/pl/test/hib/TestGit.java
@@ -0,0 +1,21 @@
+/**
+ * 
+ */
+package pl.test.hib;
+
+/**
+ * @author Primetals 2017 -- Wieslaw Czuber
+ *
+ */
+public class TestGit {
+
+  /**
+   * @param args
+   */
+  public static void main(String[] args) {
+    // TODO Auto-generated method stub
+
+    System.out.println("Test GitHub");
+  }
+
+}
